148051e45aa40e39c76b50e06222f8915ab99c96
tanvitandon67-web <tanvitandon67@gmail.com>
2026-02-26 19:16:26 +1100

    Add files via upload

---
+++ check_number.py
@@ -0,0 +1,7 @@
+num = int( input ("enter a number to check if number is greater than 100 = "))
+if num > 100:
+    print (" I am in if block")
+    print("number is greater then 100")
+else :
+    print("I am in else block")
+    print("number is not greater than 100")
