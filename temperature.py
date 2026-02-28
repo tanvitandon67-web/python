992f1c52659531b40c12f66d090709e7573becf1
tanvitandon67-web <tanvitandon67@gmail.com>
2026-02-28 20:31:16 +1100

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,7 @@
+weather = int(input("enter the weather = "))
+if weather < 20 :
+    print ("jacket requrired")
+    print("you are able to wear dark clothes and jackets")
+else :
+    print ("jacket not required ")
+    print (" you are able to wear light clothes")
