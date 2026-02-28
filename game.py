8fd03d12a581b7a026174cab80d6870c7a5b882c
tanvitandon67-web <tanvitandon67@gmail.com>
2026-02-28 20:51:33 +1100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,11 @@
+num = int(input("enter a number between 1 to 100 = "))
+if num < 50 :
+    if num < 25 :
+        print("number is less then 25 ")  
+    else  :
+        print("number is greater than 26 and less than 49")   
+else :
+    if num < 75 :
+        print(' number is greater than 50 and less that 74')       
+    else  :
+        print("number is greater than 75 and less than 100")
