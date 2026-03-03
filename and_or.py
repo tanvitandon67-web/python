b40705dae10068a09ff0ad50d187ea4109fb0911
tanvitandon67-web <tanvitandon67@gmail.com>
2026-03-03 17:26:03 +1100

    Add files via upload

---
+++ and_or.py
@@ -0,0 +1,11 @@
+a = 10
+b = -20
+if a > 0 and b > 0  :
+    print("either of numbers is greater then zero  ")
+else :
+    print("only one  number is greater then zero")  
+
+if a > 0 or  b > 0  :
+    print("only one number is greater then zero ")
+else :
+    print("none of the numbers are greater then zero")    
