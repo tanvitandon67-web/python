de2ec17ecbd99014b07d71b31fe56bf895a8622e
tanvitandon67-web <tanvitandon67@gmail.com>
2026-03-03 17:39:39 +1100

    Add files via upload

---
+++ Not.py
@@ -0,0 +1,17 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b) 
+print(b != c)
+a = " python"
+b = "coding "
+if a != b :
+    print("python and coding are both diffrent ")
+a = 4
+b = 5
+if (a == 1 ) != (b == 5 ) :
+    print("hello")
+a = int (input ("enter a number"))
+if a % 2 != 0 :
+    print ("the number is odd")
