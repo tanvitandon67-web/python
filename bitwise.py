5ee0d19ba33ca8820bcaf95a974588d72b5451f1
tanvitandon67-web <tanvitandon67@gmail.com>
2026-03-05 19:24:14 +1100

    Add files via upload

---
+++ bitwise.py
@@ -0,0 +1,8 @@
+x = 10 
+y = -10
+print(" after shifting one bit right",x >> 1)
+print(" after shifting one bit right",y >> 1)
+a = 5
+b = -5
+print(" after shifting one bit left",a << 1)
+print(" after shifting one bit left",b  << 1)
