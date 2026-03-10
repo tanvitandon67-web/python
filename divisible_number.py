ca9e7ff5b21bc074d24adce59065b511f1b9385f
tanvitandon67-web <tanvitandon67@gmail.com>
2026-03-10 17:34:56 +1100

    Add files via upload

---
+++ divisible_number.py
@@ -0,0 +1,6 @@
+numn = int(input("enter your numaretor"))
+numd = int(input("enter your denomonator"))
+if numn % numd == 0:
+    print("number is divisible ")
+else :
+    print ("number is not divisible")   
