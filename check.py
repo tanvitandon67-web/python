c1ad65bf0b7edc8639811b65e05805df59d54452
tanvitandon67-web <tanvitandon67@gmail.com>
2026-03-16 20:25:23 +1100

    Add files via upload

---
+++ check.py
@@ -0,0 +1,7 @@
+age = int(input("enter your age please = "))
+if age <= 10:
+ print("you are not allowed in class")
+elif age >= 20 :
+ print("you are not allowed in class")
+else :
+ print("you are allowed,please wait for your email to come")
