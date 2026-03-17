f07622371b402eba35051ce18f5c81600d4cb250
tanvitandon67-web <tanvitandon67@gmail.com>
2026-03-17 17:54:17 +1100

    Add files via upload

---
+++ power.py
@@ -0,0 +1,6 @@
+number  = int(input("enter a number  = "))
+power = int(input("enter your power = "))
+result = 1
+for i in range (power) :
+    result = result * number
+print (" your answer is",result ) 
