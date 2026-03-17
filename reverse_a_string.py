d9abf4210345781b694faf46947e918f8b95d5b7
tanvitandon67-web <tanvitandon67@gmail.com>
2026-03-17 17:37:45 +1100

    Add files via upload

---
+++ reverse_a_string.py
@@ -0,0 +1,7 @@
+print("take any string from user and reverse the string  ")
+string = input("enter your string = ")
+string2 = (" ")
+for i in string :
+    string2 = i + string2
+print("the orginal string is equal to", string)
+print("the reversed  string is equal to", string2 )
