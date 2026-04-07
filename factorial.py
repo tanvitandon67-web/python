9e9de4b13a6df5cfda028b4958f4a1f23db3d09c
tanvitandon67-web <tanvitandon67@gmail.com>
2026-04-07 16:39:38 +1000

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,11 @@
+def factorial(x) :
+    """ this is a recursive function to find the factorial of an integer"""
+    if x == 0 or x == 1 :
+        return 1
+    else : 
+        return x * factorial(x - 1)
+print(factorial.__doc__)
+print("factorial of x = ",factorial(6))
+
+
+    
