a16c628899d65f6b0f8914902f1048214d39c8d9
tanvitandon67-web <tanvitandon67@gmail.com>
2026-04-14 18:06:35 +1000

    Add files via upload

---
+++ value_error.py
@@ -0,0 +1,8 @@
+try :
+    num = int(input("enter a number ="))
+    print("the number is equal to =",num)
+except ValueError as ex :
+    print ("YOU HAVE ENTERED THE WRONG VALUE",ex)
+    
+
+    
