a4a84f4918e8e27096d750fb49bb3a6bcda6f84d
tanvitandon67-web <tanvitandon67@gmail.com>
2026-04-15 14:11:50 +1000

    Add files via upload

---
+++ change.py
@@ -0,0 +1,10 @@
+
+def change (a,b) :
+    return (a - b)
+a = float(input("enter the money you gave ="))
+b = float(input("enter the price of the product = "))
+
+sum = change(a,b)
+
+
+print("The amount the shop keeper needs to give is " ,sum)
