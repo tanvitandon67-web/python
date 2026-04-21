ceb6f817cfed37b498d2397cb11bc815d43bf9b2
tanvitandon67-web <tanvitandon67@gmail.com>
2026-04-21 18:35:25 +1000

    Add files via upload

---
+++ math.py
@@ -0,0 +1,17 @@
+import math
+print("the floor and ceiling value 23.56")
+print(str(math.ceil(23.56)))
+print( str(math.floor(23.56)))
+
+x = 10
+y = - 10
+print("the value of x after copying the sign of y ")
+print(str(math.copysign(x,y)))
+
+print("absolulate value - 96 and 56 ")
+print(str(math.fabs(-96)))
+print(str(math.fabs(56)))
+
+print("the hcf of 24 and 56 is = ")
+print(str(math.gcd(24,56)))
+
