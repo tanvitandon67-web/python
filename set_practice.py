c51659033c1605dd8b794c341dc713e2daebc355
tanvitandon67-web <tanvitandon67@gmail.com>
2026-05-12 18:18:27 +1000

    Add files via upload

---
+++ set_practice.py
@@ -0,0 +1,14 @@
+set1 = {2,4,6,8,10,12,14,16,}
+set2 = {6,7,8,9,10,11,12}
+
+set3 = set1.union(set2)
+print(set3)
+
+set4 = set1.intersection(set2)
+print(set4)
+
+set5 = set1.difference(set2)
+print(set5)
+
+set6 = set1.symmetric_difference(set2)
+print(set6)
