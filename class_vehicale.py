95aeee0d4ece8258699521c1e9e17d72f881e388
tanvitandon67-web <tanvitandon67@gmail.com>
2026-05-21 18:22:35 +1000

    Add files via upload

---
+++ class_vehicale.py
@@ -0,0 +1,21 @@
+class Vehicle :
+    def __init__(self,maxium,mileage):
+        self.maxium = maxium
+        self.mileage = mileage
+
+
+modelX = Vehicle(240,18)
+modelY = Vehicle(300,10)
+
+
+print("ModelX maxium speed : ",modelX.maxium)
+print("ModelX mileage speed : ",modelX.mileage)
+
+print("ModelY maxium speed : ",modelY.maxium)
+print("ModelY mileage speed : ",modelY.mileage)
+
+
+
+    
+
+
