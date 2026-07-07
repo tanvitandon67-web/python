f1346c5723c18dc852607a69674758de3bcd0b9d
tanvitandon67-web <tanvitandon67@gmail.com>
2026-07-07 18:40:00 +1000

    Add files via upload

---
+++ LoginApp.py
@@ -0,0 +1,5 @@
+from tkinter import *
+
+root = Tk()
+root.title('login app')
+root.geometry('400x400')
---
+++ NumberPad.py
@@ -0,0 +1,5 @@
+from tkinter import *
+
+root = Tk()
+root.title('Number Pad')
+root.geometry
